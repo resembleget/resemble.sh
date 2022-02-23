6aa27ca03adace49e3a0c309316413782a7d6218
resembleget <100261154+resembleget@users.noreply.github.com>
2022-02-23 10:35:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-url = 'https://api.ipify.org/?format=json';
-var ip = document.getElementById("ip");
-function find(){
-    fetch(url).then(res => res.json().then(result => {
-        ip.innerText = result.ip;
-    }))
-}
